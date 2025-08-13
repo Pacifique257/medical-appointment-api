49b9e33338f0f18a2714bdd5c05395a4ce2251d7
pacifique257 <pacifiquemugisha079@gmail.com>
2025-08-13 17:25:47 +0200

    Utiliser chemin absolu pour docker dans build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Checkout
-git fetch --all
-git checkout main
-git pull origin main
+/usr/bin/git fetch --all
+/usr/bin/git checkout main
+/usr/bin/git pull origin main
 
 # Build Maven
-docker run --rm -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.9.3-eclipse-temurin-17 mvn clean package -DskipTests=false
+/usr/bin/docker run --rm -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.9.3-eclipse-temurin-17 mvn clean package -DskipTests=false
 
 # Run Tests
-docker run --rm -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.9.3-eclipse-temurin-17 mvn test
+/usr/bin/docker run --rm -v "$PWD":/usr/src/mymaven -w /usr/src/mymaven maven:3.9.3-eclipse-temurin-17 mvn test
 
 # Build Docker Image
 DOCKER_IMAGE="pacifiquedev/medical-appointment"
-docker build -t "$DOCKER_IMAGE":latest .
-commit=$(git rev-parse --short HEAD)
-docker tag "$DOCKER_IMAGE":latest "$DOCKER_IMAGE":$commit
+/usr/bin/docker build -t "$DOCKER_IMAGE":latest .
+commit=$(/usr/bin/git rev-parse --short HEAD)
+/usr/bin/docker tag "$DOCKER_IMAGE":latest "$DOCKER_IMAGE":$commit
 
 # Push Docker Image
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "$DOCKER_IMAGE":latest
-docker push "$DOCKER_IMAGE":$commit
+echo "$DOCKER_PASSWORD" | /usr/bin/docker login -u "$DOCKER_USERNAME" --password-stdin
+/usr/bin/docker push "$DOCKER_IMAGE":latest
+/usr/bin/docker push "$DOCKER_IMAGE":$commit
