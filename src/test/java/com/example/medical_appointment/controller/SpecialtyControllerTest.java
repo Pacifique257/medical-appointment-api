62cb21d3800f30a08980121ad2f97a9b2d83bab5
pacifique257 <pacifiquemugisha079@gmail.com>
2025-08-30 17:54:32 +0200

    correction des erreur dans SpecialityControllerTest et AvaillabilityControllerTest

--- src/test/java/com/example/medical_appointment/controller/SpecialtyControllerTest.java
+++ src/test/java/com/example/medical_appointment/controller/SpecialtyControllerTest.java
@@ -11,9 +11,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
